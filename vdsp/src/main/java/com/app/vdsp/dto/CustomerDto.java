b6fdcaea2dddc63c755e724ebd6474d7945a783e
panchaliSam <panchalisam20030117@gmail.com>
2025-03-20 21:28:08 +0530

    Create customer dto

--- vdsp/src/main/java/com/app/vdsp/dto/CustomerDto.java
+++ vdsp/src/main/java/com/app/vdsp/dto/CustomerDto.java
@@ -1,36 +1,23 @@
-package com.app.vdsp.entity;
+package com.app.vdsp.dto;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-@Table(name = "customer", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
-public class Customer {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class CustomerDto {
     @NotBlank(message = "First name should not be blank")
     private String firstName;
     @NotBlank(message = "Last name should not be blank")
     private String lastName;
-    @Column(nullable = false, unique = true)
+    @Email
     @NotBlank(message = "Email should not be blank")
-    @Email(message = "Invalid email format")
-    @Pattern(
-            regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$",
-            message = "Invalid email pattern"
-    )
     private String email;
     @NotBlank(message = "Password should not be blank")
     @Size(min = 60)
