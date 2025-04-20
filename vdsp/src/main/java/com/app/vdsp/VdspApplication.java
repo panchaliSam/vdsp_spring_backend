1b28195bafb725cba1edb01a90954d4ec62779b6
panchaliSam <panchalisam20030117@gmail.com>
2025-04-20 19:02:21 +0530

    Set home controller

--- vdsp/src/main/java/com/app/vdsp/VdspApplication.java
+++ vdsp/src/main/java/com/app/vdsp/VdspApplication.java
@@ -2,12 +2,12 @@ package com.app.vdsp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class VdspApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VdspApplication.class, args);
 	}
-
 }
---
+++ vdsp/src/main/java/com/app/vdsp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.app.vdsp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to the App!";
+    }
+}
