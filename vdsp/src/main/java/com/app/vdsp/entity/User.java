9dfb6d06feba76621286c880a2409b73c7db15de
panchaliSam <panchalisam20030117@gmail.com>
2025-04-24 22:52:41 +0530

    Update the user table strcuture

--- vdsp/src/main/java/com/app/vdsp/entity/User.java
+++ vdsp/src/main/java/com/app/vdsp/entity/User.java
@@ -10,10 +10,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -22,15 +26,20 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 @Table(name =" users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User implements UserDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @NotBlank(message = "First name should not be blank")
     private String firstName;
+
     @NotBlank(message = "Last name should not be blank")
     private String lastName;
+
     @Column(nullable = false, unique = true)
     @NotBlank(message = "Email should not be blank")
     @Email(message = "Invalid email format")
@@ -39,14 +48,24 @@ public class User implements UserDetails {
             message = "Invalid email pattern"
     )
     private String email;
+
     @NotBlank(message = "Password should not be blank")
     @Size(min = 60)
     private String password;
+
     @NotBlank(message = "Phone number should not be blank")
     @Pattern(regexp = "^[0-9]{10}$", message = "Phone number must be 10 digits")
     private String phoneNumber;
+
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
