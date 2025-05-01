0319156da5e4f0dd5ccc076a36437bf4496b30b3
panchaliSam <panchalisam20030117@gmail.com>
2025-05-01 18:52:40 +0530

    Create update approval satatus method

--- vdsp/src/main/java/com/app/vdsp/service/Impl/ReservationApprovalServiceImpl.java
+++ vdsp/src/main/java/com/app/vdsp/service/Impl/ReservationApprovalServiceImpl.java
@@ -4,12 +4,14 @@ import com.app.vdsp.dto.ReservationApprovalDto;
 import com.app.vdsp.entity.ReservationApproval;
 import com.app.vdsp.repository.ReservationApprovalRepository;
 import com.app.vdsp.service.ReservationApprovalService;
+import com.app.vdsp.type.ApprovalStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -39,4 +41,25 @@ public class ReservationApprovalServiceImpl implements ReservationApprovalServic
                         "Error while getting reservation approvals", e);
         }
     }
+
+    @Override
+    public ReservationApprovalDto updateApprovalStatus(Long id, ApprovalStatus status) {
+        try {
+            ReservationApproval reservationApproval = reservationApprovalRepository.findById(id)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Reservation approval not found"));
+
+            reservationApproval.setStatus(status == ApprovalStatus.APPROVED);
+            reservationApproval.setApprovedAt(LocalDateTime.now());
+
+            ReservationApproval updatedApproval = reservationApprovalRepository.save(reservationApproval);
+            log.info("Reservation approval updated: {}", updatedApproval);
+
+            return ReservationApprovalDto.fromEntity(updatedApproval);
+        } catch (Exception e) {
+            log.error("Error while updating reservation approval status", e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    "Error while updating reservation approval status", e);
+        }
+    }
+
 }
--- vdsp/src/main/java/com/app/vdsp/service/ReservationApprovalService.java
+++ vdsp/src/main/java/com/app/vdsp/service/ReservationApprovalService.java
@@ -1,9 +1,11 @@
 package com.app.vdsp.service;
 
 import com.app.vdsp.dto.ReservationApprovalDto;
+import com.app.vdsp.type.ApprovalStatus;
 
 import java.util.List;
 
 public interface ReservationApprovalService {
     List<ReservationApprovalDto> getAllReservationApprovals();
+    ReservationApprovalDto updateApprovalStatus(Long id, ApprovalStatus status);
 }
