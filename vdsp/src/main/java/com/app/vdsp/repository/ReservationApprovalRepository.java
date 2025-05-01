b7f144a0a1ed1b2471352434e6c4334dba409682
panchaliSam <panchalisam20030117@gmail.com>
2025-05-01 17:48:50 +0530

    Resrvation approval repository

---
+++ vdsp/src/main/java/com/app/vdsp/repository/ReservationApprovalRepository.java
@@ -0,0 +1,7 @@
+package com.app.vdsp.repository;
+
+import com.app.vdsp.entity.ReservationApproval;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationApprovalRepository extends JpaRepository<ReservationApproval, Long> {
+}
