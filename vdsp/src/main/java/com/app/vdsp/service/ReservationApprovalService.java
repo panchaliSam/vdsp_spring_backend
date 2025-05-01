0ab1de6664022e1e03f595d0d5893077db776ef6
panchaliSam <panchalisam20030117@gmail.com>
2025-05-01 18:18:52 +0530

    Add getAllResrvationApproval method

--- vdsp/src/main/java/com/app/vdsp/service/ReservationApprovalService.java
+++ vdsp/src/main/java/com/app/vdsp/service/ReservationApprovalService.java
@@ -1,4 +1,9 @@
 package com.app.vdsp.service;
 
+import com.app.vdsp.dto.ReservationApprovalDto;
+
+import java.util.List;
+
 public interface ReservationApprovalService {
+    List<ReservationApprovalDto> getAllReservationApprovals();
 }
